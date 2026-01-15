fef40959b825db25271f99ce4fa72cf067fdf0f9
Adise <admin@pauibanez.me>
2026-01-15 08:43:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CPP Template
+
+This rrepo contains a ccpp project template for VSCode with a dev pipeline
---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+if not exist build mkdir build
+if not exist build\objects mkdir build\objects
+
+setlocal enabledelayedexpansion
+
+call node ./build.js
+endlocal
+
+if errorlevel 1 (
+  echo Build failed.
+  exit /b %errorlevel%
+) else (
+  echo Build succeeded. 
+  cls
+)
+
